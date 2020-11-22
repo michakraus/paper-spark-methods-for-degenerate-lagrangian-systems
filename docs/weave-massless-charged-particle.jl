3efcb07e6027692e1a5e4578dfd9c78b4254e5e1
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-11-22 21:49:27 +0100

    Move weave scripts into separete directory.

--- docs/weave-massless-charged-particle.jl
+++ docs/weave-massless-charged-particle.jl
@@ -7,37 +7,37 @@ Weave.set_chunk_defaults!(:echo => false, :results => "raw")
 
 include("../src/massless-charged-particle.jl")
 
-weave("../massless-charged-particle-spark-glvprk.jmd",
+weave("../weave/massless-charged-particle-spark-glvprk.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-glspark.jmd",
+weave("../weave/massless-charged-particle-spark-glspark.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-lobspark.jmd",
+weave("../weave/massless-charged-particle-spark-lobspark.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-pinternal.jmd",
+weave("../weave/massless-charged-particle-spark-pinternal.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-plobatto.jmd",
+weave("../weave/massless-charged-particle-spark-plobatto.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-pmidpoint.jmd",
+weave("../weave/massless-charged-particle-spark-pmidpoint.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
 
-weave("../massless-charged-particle-spark-psymmetric.jmd",
+weave("../weave/massless-charged-particle-spark-psymmetric.jmd",
          out_path = "src/massless-charged-particle",
          doctype = "github",
          mod = MasslessChargedParticleSPARK)
--- lotka-volterra-2d-spark.jl
+++ lotka-volterra-2d-spark.jl
@@ -5,37 +5,37 @@ Weave.set_chunk_defaults!(:echo => false, :results => "raw")
 
 include("src/lotka-volterra-2d.jl")
 
-weave("lotka-volterra-2d-spark-glvprk.jmd",
+weave("weave/lotka-volterra-2d-spark-glvprk.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-glspark.jmd",
+weave("weave/lotka-volterra-2d-spark-glspark.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-lobspark.jmd",
+weave("weave/lotka-volterra-2d-spark-lobspark.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-pinternal.jmd",
+weave("weave/lotka-volterra-2d-spark-pinternal.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-pmidpoint.jmd",
+weave("weave/lotka-volterra-2d-spark-pmidpoint.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-psymmetric.jmd",
+weave("weave/lotka-volterra-2d-spark-psymmetric.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
 
-weave("lotka-volterra-2d-spark-plobatto.jmd",
+weave("weave/lotka-volterra-2d-spark-plobatto.jmd",
          out_path = "build/lotka-volterra-2d-spark",
          doctype = "github",
          mod = LotkaVolterra2dSingularSPARK)
